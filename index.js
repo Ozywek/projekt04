da4140bb6f4bbaf6f8de8e5c8d0636de5fc25a2f
Mikołaj <dlugaibezsensownanazwa@gmail.com>
2026-03-03 23:07:54 +0000

    testowanie nodowv2

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.set('view engine', 'ejs');
 app.get("/", (req, res) => {
   res.render("index", {
     title: "TEST",
-    theme: "dark"
+    theme: "dark",
+    lorem: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sagittis mi massa. Sed hendrerit mi non elit auctor, at posuere purus convallis. Nam tristique at metus in iaculis. Pellentesque hendrerit facilisis gravida. Sed posuere fermentum arcu sit amet mattis. Cras dignissim erat neque, et vestibulum tellus imperdiet scelerisque. Pellentesque turpis libero, vulputate in varius in, lacinia nec nibh. In elit felis, tempus quis efficitur vel, lacinia vel massa. Quisque dignissim consequat semper"
+
   });
 });
 
